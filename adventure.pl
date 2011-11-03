3910e4b67821d75d209960f17275f7448379a77f
Jan Keromnes <janx@linux.com>
2011-11-03 13:05:24 +0100

    Work in progress.
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- adventure.pl
+++ adventure.pl
@@ -1,33 +1,50 @@
-goto(X) :- 
-  location(you, L), 
-  connect(L, X), 
-  retract( location(you, L) ), 
-  assert( location(you, X) ), 
-  write($ You are in the $), write(X), nl. 
-goto(X) :- 
-  write($ You can't get there from here. $), nl. 
+location(egg, duck_pen). 
+location(ducks, duck_pen). 
+location(fox, woods). 
+location(you, house). 
+nextto(duck_pen, yard, closed). 
+nextto(yard, house, open). 
+nextto(yard, woods, open). 
+connect(X, Y) :- nextto(X, Y, opened). 
+connect(X, Y) :- nextto(Y, X, opened).
 
-open(gate) :- 
-  1assert( nextto(duck_pen, yard, open ) ). 
-close(gate) :- 
-  retract( nextto(duck_pen, yard, _) ), 
-assert( nextto(duck_pen, yard, closed) ). 
 
-chase(ducks) :- 
-  location(ducks, yard), 
-  location(you, yard), 
-  retract( location(ducks, yard) ), 
-  assert( location(ducks, duck_pen) ), 
-  write($ The ducks are back in their pen. $), nl. 
-chase(ducks) :- 
-  write($ No ducks here. $), nl. 
+goto(X) :-
+  location(you, L),
+  connect(L, X),
+  retract( location(you, L) ),
+  assert( location(you, X) ),
+  write(' You are in the '), write(X), nl.
+
+you_have(nothing).
+
+goto(X) :-
+  write(' You can\'t get there from here. '), nl.
+
+opengate(gate) :-
+  assert( nextto(duck_pen, yard, opened ) ).
+
+closegate(gate) :-
+  retract( nextto(duck_pen, yard, _) ),
+  assert( nextto(duck_pen, yard, closed) ).
+
+chase(ducks) :-
+  location(ducks, yard),
+  location(you, yard),
+  retract( location(ducks, yard) ),
+  assert( location(ducks, duck_pen) ),
+  write(' The ducks are back in their pen. '), nl.
+
+chase(ducks) :-
+  write(' No ducks here. '), nl.
+
 take(X) :- 
   location(you, L), 
   retract( location(X, L) ), 
   assert( you_have(X) ), 
-  write($ You now have the $), write(X), nl. 
+  write(' You now have the '), write(X), nl. 
 take(X) :- 
-  write($ There is no $), write(X), write($ here.$), nl. 
+  write(' There is no '), write(X), write(' here.'), nl. 
 
 ducks :- 
   location(ducks, duck_pen), 
@@ -35,17 +52,17 @@ ducks :-
   connect(duck_pen, yard), 
   retract( location(ducks, duck_pen) ), 
   assert( location(ducks, yard) ), 
-  write($ The ducks have run into the yard. $), nl.
+  write(' The ducks have run into the yard. '), nl.
 ducks. 
 fox :- 
   location(ducks, yard), 
   location(you, house), 
-  write($ The fox has taken a duck. $), nl. 
+  write(' The fox has taken a duck. '), nl. 
 fox. 
 
 go :- done. 
 go :- 
-  write($>> $), 
+  write('>> '), 
   read(X), 
   call(X), 
   ducks, 
@@ -54,4 +71,5 @@ go :-
 done :- 
   location(you, house), 
   you_have(egg), 
-  write($ Thanks for getting the egg. $), nl. 
+  write(' Thanks for getting the egg. '), nl.
+
--- dots.pl
+++ dots.pl
@@ -1,51 +1,6 @@
-goto(X) :- 
-  location(you, L), 
-  connect(L, X), 
-  retract( location(you, L) ), 
-  assert( location(you, X) ), 
-  write($ You are in the $), write(X), nl. 
-goto(X) :- 
-  write($ You can't get there from here. $), nl. 
-
-open(gate) :- 
-  1assert( nextto(duck_pen, yard, open ) ). 
-close(gate) :- 
-  retract( nextto(duck_pen, yard, _) ), 
-assert( nextto(duck_pen, yard, closed) ). 
-
-chase(ducks) :- 
-  location(ducks, yard), 
-  location(you, yard), 
-  retract( location(ducks, yard) ), 
-  assert( location(ducks, duck_pen) ), 
-  write($ The ducks are back in their pen. $), nl. 
-chase(ducks) :- 
-  write($ No ducks here. $), nl. 
-take(X) :- 
-  location(you, L), 
-  retract( location(X, L) ), 
-  assert( you_have(X) ), 
-  write($ You now have the $), write(X), nl. 
-take(X) :- 
-  write($ There is no $), write(X), write($ here.$), nl. 
-
-ducks :- 
-  location(ducks, duck_pen), 
-  location(you, duck_pen), 
-  connect(duck_pen, yard), 
-  retract( location(ducks, duck_pen) ), 
-  assert( location(ducks, yard) ), 
-  write($ The ducks have run into the yard. $), nl.
-ducks. 
-fox :- 
-  location(ducks, yard), 
-  location(you, house), 
-  write($ The fox has taken a duck. $), nl. 
-fox. 
-
-go :- done. 
-go :- 
-  write($>> $), 
+go :- done.
+go :-
+  write(">> "), 
   read(X), 
   call(X), 
   ducks, 
@@ -54,4 +9,4 @@ go :-
 done :- 
   location(you, house), 
   you_have(egg), 
-  write($ Thanks for getting the egg. $), nl. 
+  write("Thanks for getting the egg."), nl. 
